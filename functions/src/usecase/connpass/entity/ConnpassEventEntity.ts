4a235584dc19484aa9e75e92e331bd6068e86289
Fumiya Kume <fumiya.kume@hotmail.com>
2019-03-26 03:37:00 +0900

    イベントの参加可能人数が存在する

--- functions/src/usecase/connpass/entity/ConnpassEventEntity.ts
+++ functions/src/usecase/connpass/entity/ConnpassEventEntity.ts
@@ -12,6 +12,7 @@ class ConnpassEventEntity {
     hash_tag: String = ""
     started_at: Date = new Date(1900, 0, 1, 1, 0, 0, 0)
     ended_at: Date = new Date(1900, 0, 1, 1, 0, 0, 0)
+    limit: number = -1
 
     constructor() {
     }
@@ -47,6 +48,9 @@ mocha.describe('Connpassの勉強会を表現するEntityが存在する', () =>
     })
     it('イベントの終了日時が存在する', () => {
         chai.expect(connpassEventEntity.ended_at).to.be.not.null
+    })
+    it('イベントの参加可能人数が存在する', () => {
+        chai.expect(connpassEventEntity.limit).to.be.equal(-1)
     })
     it('イベントIDを使いイベントを比較することができる', () => {
         const event1 = new ConnpassEventEntity()
