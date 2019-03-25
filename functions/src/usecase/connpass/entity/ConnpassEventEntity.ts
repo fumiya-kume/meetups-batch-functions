8273dd8d5528d188b8033dc0a8635671bda811e9
Fumiya Kume <fumiya.kume@hotmail.com>
2019-03-25 17:30:22 +0900

    ConnpassのイベントにはイベントIDが存在する

--- functions/src/usecase/connpass/entity/ConnpassEventEntity.ts
+++ functions/src/usecase/connpass/entity/ConnpassEventEntity.ts
@@ -2,8 +2,10 @@ import * as mocha from "mocha"
 import * as chai from 'chai'
 
 class ConnpassEventEntity {
+    event_id: number
     title: String
     constructor() {
+        this.event_id = 0
         this.title = ""
     }
 }
@@ -13,6 +15,12 @@ mocha.describe('Connpassの勉強会を表現するEntityが存在する', () =>
     it('Connpassのイベントにはタイトルが存在する', () => {
         connpassEventEntity.title = ""
         chai.expect(connpassEventEntity.title).equal("")
-        
+    })
+    it('イベントIDが存在する', () => {
+        chai.expect(connpassEventEntity.event_id).to.be.equal(0)
+    })
+    it('イベントIDを設定することができる', () =>{
+        connpassEventEntity.event_id = 1
+        chai.expect(connpassEventEntity.event_id).to.be.equal(1)
     })
 })
