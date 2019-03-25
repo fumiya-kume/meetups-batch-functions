cb58e298d9238ecf44ed5369149c40ecc9f1938b
Fumiya Kume <fumiya.kume@hotmail.com>
2019-03-25 17:06:33 +0900

    Type Script で Unit Test できるようになった

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,18 @@
+import * as mocha from "mocha"
+import * as chai from 'chai'
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+mocha.describe('DESC', () => {
+    it('IT', () => {
+        chai.assert("foo", "bar");
+    })
+    it('ON', () => {
+        chai.assert("foo", "bar");
+    });
+});
